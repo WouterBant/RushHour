0a16d75445ac13fb8f5503c54f7fce14fc58e95e
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-01-09 12:19:52 +0100

    board method 1 step

--- Code/board.py
+++ Code/board.py
@@ -1,4 +1,44 @@
+from car import Car
+
 class Board:
 
-    def __init__(self):
+    def __init__(self, dim: int) -> None:
+        self.dim = dim
+        self.board = [["."] * dim ] * dim
+
+    def placeCar(self, car: Car, row: int, col: int) -> None:
+        carName = car.name
+        carOrientation = car.orientation
+        carLength = car.length
+        if carOrientation == "H":
+            for c in range(col, col + carLength):
+                self.board[row][c] = carName
+        else:
+            for r in range(row, row + carLength):
+                self.board[r][col] = carName
+        print(self.board)
+        
+    
+    def moveCarOne(self, car, dir):
+        if car.orientation == "V":
+            if dir == "Down":
+                if car.row + car.length + 1 < self.dim and self.board[car.row + car.length + 1][car.col] == ".":
+                    self.board[car.row][car.col] = "."
+                    car.row += 1
+                    self.board[car.row + car.length][car.col] = car.name
+                    return True
+                return False
+            else:
+                if car.row - 1 >= 0 and self.board[car.row - 1][car.col] == ".":
+                    self.board[car.row + car.length][car.col] = "."
+                    car.row -= 1
+                    self.board[car.row][car.col] = car.name
+                    return True
+                return False
+        else:
+            pass
+
+        
+
+    def moveCarFar(self, car):
         pass
