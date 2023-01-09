3991a14785e3ecccb43131cbdef386345560baab
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-01-09 11:23:46 +0100

    classes added for car and board

---
+++ Code/car.py
@@ -0,0 +1,9 @@
+
+class Car:
+
+    def __init__(self, name: str, orientation: str, row: int, col: int, length: int) -> None:
+        self.name = name
+        self.orientation = orientation
+        self.row = row
+        self.col = col
+        self.length = length
