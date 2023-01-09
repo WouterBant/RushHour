3539990559255597446ef1aab311158bfc9d6410
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-01-09 17:29:23 +0100

    docstrings added + flake8 good

--- Code/board.py
+++ Code/board.py
@@ -9,42 +9,45 @@ class Board:
         self.board = [["." for _ in range(size)] for _ in range(size)]
 
     def placeCar(self, car: Car, row: int, col: int) -> None:
-        if car.orientation == "H":
+        """
+        Places the given car at its initial position.
+        """
+        if car.orientation == 'H':
             for c in range(col, col + car.length):
                 self.board[row][c] = car.name
-        elif car.orientation == "V":
+        elif car.orientation == 'V':
             for r in range(row, row + car.length):
                 self.board[r][col] = car.name
-        if car.name == "X":
+        if car.name == 'X':
             self.exitRow = car.row
 
     def moveCarOne(self, car: Car, direction: str) -> bool:
         """
         Tries to move the car in the given direction, returns True if possible else False.
         """
-        if car.orientation == "V":
-            if direction == "Down":
-                if car.row + car.length < self.size and self.board[car.row + car.length][car.col] == ".":
-                    self.board[car.row][car.col] = "."
+        if car.orientation == 'V':
+            if direction == 'Down':
+                if car.row + car.length < self.size and self.board[car.row + car.length][car.col] == '.':
+                    self.board[car.row][car.col] = '.'
                     car.row += 1
                     self.board[car.row + car.length - 1][car.col] = car.name
                     return True
-            elif direction == "Up":
-                if car.row - 1 >= 0 and self.board[car.row - 1][car.col] == ".":
-                    self.board[car.row + car.length - 1][car.col] = "."
+            elif direction == 'Up':
+                if car.row - 1 >= 0 and self.board[car.row - 1][car.col] == '.':
+                    self.board[car.row + car.length - 1][car.col] = '.'
                     car.row -= 1
                     self.board[car.row][car.col] = car.name
                     return True
-        elif car.orientation == "H":
-            if direction == "Right":
-                if car.col + car.length < self.size and self.board[car.row][car.col + car.length] == ".":
-                    self.board[car.row][car.col] = "."
+        elif car.orientation == 'H':
+            if direction == 'Right':
+                if car.col + car.length < self.size and self.board[car.row][car.col + car.length] == '.':
+                    self.board[car.row][car.col] = '.'
                     car.col += 1
                     self.board[car.row][car.col + car.length - 1] = car.name
                     return True
-            elif direction == "Left":
-                if car.col - 1 >= 0 and self.board[car.row][car.col - 1] == ".":
-                    self.board[car.row][car.col + car.length - 1] = "."
+            elif direction == 'Left':
+                if car.col - 1 >= 0 and self.board[car.row][car.col - 1] == '.':
+                    self.board[car.row][car.col + car.length - 1] = '.'
                     car.col -= 1
                     self.board[car.row][car.col] = car.name
                     return True
@@ -56,9 +59,12 @@ class Board:
         """
         while self.moveCarOne(car, direction):
             pass
-    
+
     def isSolved(self) -> bool:
-        return self.board[self.exitRow][self.size - 1] == "X"
+        """
+        Return True if the red car is at the exit.
+        """
+        return self.board[self.exitRow][self.size - 1] == 'X'
 
     def __str__(self) -> str:
         """
@@ -66,5 +72,5 @@ class Board:
         """
         boardRepresentation = ''
         for row in self.board:
-            boardRepresentation += '|' + ' '.join(row) + '|' + '\n'
+            boardRepresentation += ' '.join(row) + '\n'
         return boardRepresentation
