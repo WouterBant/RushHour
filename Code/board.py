29eeaf749938f452a24ceebd4b5dfbfc39edc93c
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-01-09 17:20:15 +0100

    isSolved method added and better variable naming

--- Code/board.py
+++ Code/board.py
@@ -4,46 +4,45 @@ from car import Car
 class Board:
     """ Stores the Rush Hour board, a '.' means empty. """
 
-    def __init__(self, dim: int) -> None:
-        self.dim = dim
-        self.board = [["." for _ in range(dim)] for _ in range(dim)]
+    def __init__(self, size: int) -> None:
+        self.size = size
+        self.board = [["." for _ in range(size)] for _ in range(size)]
 
     def placeCar(self, car: Car, row: int, col: int) -> None:
-        carName = car.name
-        carOrientation = car.orientation
-        carLength = car.length
-        if carOrientation == "H":
-            for c in range(col, col + carLength):
-                self.board[row][c] = carName
-        else:
-            for r in range(row, row + carLength):
-                self.board[r][col] = carName
+        if car.orientation == "H":
+            for c in range(col, col + car.length):
+                self.board[row][c] = car.name
+        elif car.orientation == "V":
+            for r in range(row, row + car.length):
+                self.board[r][col] = car.name
+        if car.name == "X":
+            self.exitRow = car.row
 
-    def moveCarOne(self, car: Car, dir: str) -> bool:
+    def moveCarOne(self, car: Car, direction: str) -> bool:
         """
         Tries to move the car in the given direction, returns True if possible else False.
         """
         if car.orientation == "V":
-            if dir == "Down":
-                if car.row + car.length < self.dim and self.board[car.row + car.length][car.col] == ".":
+            if direction == "Down":
+                if car.row + car.length < self.size and self.board[car.row + car.length][car.col] == ".":
                     self.board[car.row][car.col] = "."
                     car.row += 1
                     self.board[car.row + car.length - 1][car.col] = car.name
                     return True
-            elif dir == "Up":
+            elif direction == "Up":
                 if car.row - 1 >= 0 and self.board[car.row - 1][car.col] == ".":
                     self.board[car.row + car.length - 1][car.col] = "."
                     car.row -= 1
                     self.board[car.row][car.col] = car.name
                     return True
         elif car.orientation == "H":
-            if dir == "Right":
-                if car.col + car.length < self.dim and self.board[car.row][car.col + car.length] == ".":
+            if direction == "Right":
+                if car.col + car.length < self.size and self.board[car.row][car.col + car.length] == ".":
                     self.board[car.row][car.col] = "."
                     car.col += 1
                     self.board[car.row][car.col + car.length - 1] = car.name
                     return True
-            elif dir == "Left":
+            elif direction == "Left":
                 if car.col - 1 >= 0 and self.board[car.row][car.col - 1] == ".":
                     self.board[car.row][car.col + car.length - 1] = "."
                     car.col -= 1
@@ -51,13 +50,15 @@ class Board:
                     return True
         return False
 
-    def moveCarFar(self, car: Car, dir: str) -> None:
+    def moveCarFar(self, car: Car, direction: str) -> None:
         """
         Moves the car in the given direction until no move could be made.
         """
-        while True:
-            if not self.moveCarOne(car, dir):
-                break
+        while self.moveCarOne(car, direction):
+            pass
+    
+    def isSolved(self) -> bool:
+        return self.board[self.exitRow][self.size - 1] == "X"
 
     def __str__(self) -> str:
         """
@@ -65,5 +66,5 @@ class Board:
         """
         boardRepresentation = ''
         for row in self.board:
-            boardRepresentation += ' '.join(row) + '\n'
+            boardRepresentation += '|' + ' '.join(row) + '|' + '\n'
         return boardRepresentation
