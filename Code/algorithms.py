82a6729bc9fa4110054b380905daf6924ad3fee8
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-01-10 18:00:14 +0100

    random added

--- Code/algorithms.py
+++ Code/algorithms.py
@@ -1,12 +1,19 @@
 import collections
 
+def random(board):
+    path = [] 
+    while not board.isSolved():
+        board = board.randomMove()
+        path.append(board)
+    return path
+
 def bfs(board):
     visit = set()
     path = [board]
     q = collections.deque()
     q.append(path)
 
-    # As long as you have not found and there are unvisited configurations
+    # As long as you have not found and there are unvisited valid configurations
     while q:
         path = q.popleft()
         currentBoard = path[-1]
@@ -14,7 +21,7 @@ def bfs(board):
         # Check if you already have seen this board if so skip else add it to visit
         if currentBoard in visit:
             continue
-        visit.add(board)
+        visit.add(currentBoard)
 
         # When the game is solved return the winning path
         if currentBoard.isSolved():
@@ -50,5 +57,5 @@ def heuristic(board):
             costNewBoard = costCalculator(newBoard, len(path)) 
             collections.heapq.heappush(q, (costNewBoard, copyPath + [newBoard]))
 
-def costCalculator(board, steps_taken):
-    return steps_taken
+def costCalculator(board, movesMade):
+    return movesMade
