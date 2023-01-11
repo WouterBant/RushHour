e3ea1ce9ba5187c3d028ad01c36c3c9618da5a5c
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-01-11 11:16:05 +0100

    load cleanup and working for all sizes

--- Code/rushhour.py
+++ Code/rushhour.py
@@ -1,41 +1,29 @@
 from car import Car
 from board import Board
 from sys import argv
-from typing import List
 import csv
 
 
 class RushHour:
     def __init__(self, filename: str) -> None:
-
-        self.cars: List = []
+        self.cars: list[Car] = []
         self.load_cars(filename)
-        self.load_board()
+        self.load_board(int(filename[19]))  # 20'th char represents board size
 
     def load_cars(self, filename: str) -> None:
-        with open(filename) as f:
-            f.readline()
-            for line in f:
-                line = line.split(",")
-                name: str = line[0]
-                orientation: str = line[1]
-                col: int = int(line[2]) - 1
-                row: int = int(line[3]) - 1
-                length: int = int(line[4].replace("\n", ""))
-                new_car: Car = Car(
-                    name=name,
-                    orientation=orientation,
-                    col=col,
-                    row=row,
-                    length=length,
-                )
+        with open(filename) as file:
+            file.readline()  # Skip over header
+            for new_line in file.readlines():
+                line = new_line.strip("\n").split(",")
+                name, orientation = line[:2]
+                col, row, length = int(line[2])-1, int(line[3])-1, int(line[4])
+                new_car = Car(name, orientation, col, row, length)
                 self.cars.append(new_car)
 
-    def load_board(self) -> None:
-        game_board = Board(6)
+    def load_board(self, size: int) -> None:
+        game_board = Board(size)
         for car in self.cars:
-            y: int = car.row
-            x: int = car.col
+            y, x = car.row, car.col
             if car.orientation == "H":
                 for i in range(0, car.length):
                     game_board.board[y][x + i] = car.name
@@ -44,11 +32,12 @@ class RushHour:
                     game_board.board[y + j][x] = car.name
         print(game_board)
 
-    def output(self, moves):
-        with open('new_file.csv', 'w', newline='') as csvfile:
-            csv.csvwriter.writerow(['car', 'move'])
+    def output(self, moves: list[list[str]]) -> None:
+        with open('new_file.csv', 'w', newline='') as move_file:
+            move_writer = csv.writer(move_file, delimiter=',')
+            move_writer.writerow(['car', 'move'])
             for car, move in moves:
-                csv.csvwriter.writerow([car, move])
+                move_writer.writerow([car, move])
 
 
 if __name__ == "__main__":
@@ -56,5 +45,5 @@ if __name__ == "__main__":
     if len(argv) != 2:
         print("Usage: python rushhour.py [filename]")
         exit(1)
-    gamename = f"../gameboards/{argv[1]}.csv"
+    gamename = f"gameboards/{argv[1]}.csv"
     game = RushHour(gamename)
