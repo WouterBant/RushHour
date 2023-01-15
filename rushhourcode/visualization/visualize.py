ca099f1b535af690aec27061a617ccda641f9070
Anoeya29 <a.sivanathan@hotmail.com>
2023-01-15 21:55:29 +0100

    Add board loading functionality for visualization

--- rushhourcode/visualization/visualize.py
+++ rushhourcode/visualization/visualize.py
@@ -1,2 +1,9 @@
-def visualize():
-    pass
+# Import and initialize the pygame library
+import csv
+import pygame
+
+csv_file = None
+
+with open('../../output/boards_output.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    data = list(csv_reader)
