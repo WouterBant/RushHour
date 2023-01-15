cdadeebdd9c8052951ef6fbbdf7ee234bd689408
Anoeya29 <a.sivanathan@hotmail.com>
2023-01-15 22:40:37 +0100

    Change gridscale to be correct and add board parsing functionality

--- rushhourcode/visualization/visualize.py
+++ rushhourcode/visualization/visualize.py
@@ -1,6 +1,7 @@
-# Import and initialize the pygame library
+# Import and initialize libraries
 import csv
 import pygame
+import numpy as np
 
 csv_file = None
 
@@ -18,29 +19,30 @@ WHITE = (200, 200, 200)
 WINDOW_HEIGHT = 400
 WINDOW_WIDTH = 400
 
-def drawGrid():
-    blockSize = int(WINDOW_WIDTH/length) #Set the size of the grid block
-    for x in range(0, WINDOW_WIDTH, blockSize):
-        for y in range(0, WINDOW_HEIGHT, blockSize):
+def drawGrid(current_board):
+    blockSize = WINDOW_WIDTH/length
+    color_dict = dict()
+    for index_row, x in enumerate(np.arange(0, WINDOW_WIDTH, blockSize)):
+        for index_col, y in enumerate(np.arange(0, WINDOW_HEIGHT, blockSize)):
             rect = pygame.Rect(x, y, blockSize, blockSize)
             pygame.draw.rect(SCREEN, WHITE, rect, 1)
 
-# Set up the drawing window
 SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
-# Run until the user asks to quit
 running = True
 while running:
     SCREEN.fill(BLACK)
-    drawGrid()
 
-    # Did the user click the window close button?
+    number_loops = int(len(data)/length)
+
+    for i in range(0, number_loops):
+        current_board = data[i*length:i*length+length]
+        drawGrid(current_board)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Flip the display
     pygame.display.flip()
 
-# Done! Time to quit.
 pygame.quit()
