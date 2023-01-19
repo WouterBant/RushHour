84cc79761595b232692360a36f281ac75970446c
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-01-19 09:54:21 +0100

    Path compression optimized

--- rushhourcode/algorithms/shortened_path_random.py
+++ rushhourcode/algorithms/shortened_path_random.py
@@ -1,24 +1,52 @@
 from ..classes.board import Board
 from typing import List
-
+from collections import deque, defaultdict
+import copy
 
 def shortened_path_random_find(board: Board) -> List[Board]:
     """Tries random moves till a solution is found. Shortens the found path."""
-    while not board.isSolved():
-        board = board.randomMove()
+    startBoard = board
+    adj = defaultdict(set)
 
-    child_dic = {}
-    child_dic[board] = -1  # The last board does not have a child
-    while board.parentBoard:
-        parent = board.parentBoard
-        if parent in child_dic:  # Shorten path here
-            newChild = child_dic[parent]
-            child_dic[parent] = newChild
-        else:
-            child_dic[parent] = board
-        board = board.parentBoard
-    path = []
-    while board in child_dic:
-        path.append(board)
-        board = child_dic[board]
-    return path[1:]
+    for n in range(5):  # Solve it 5 times
+        board = copy.deepcopy(startBoard)
+        path = []
+        path.append(startBoard)
+        while not board.isSolved():
+            board = board.randomMove()
+            path.append(board)
+
+        for par, child in zip(path, path[1:]):
+            adj[par].add(child)
+        print(f"Solved {n}")
+        print(f"Pathlength: {len(path)}")
+
+    # Do a bfs on the nodes retrieved from the n random solutions
+    visit = set()
+    visit.add(startBoard)
+    q = deque([startBoard])
+    depth = 0
+    while q:
+        for _ in range(len(q)):
+            currentBoard = q.popleft()
+
+            # When the game is solved return the winning path
+            if currentBoard.isSolved():
+                path = []
+                path.append(currentBoard)
+                # Create the path by traversing back in the graph
+                while currentBoard.parentBoard:
+                    currentBoard = currentBoard.parentBoard
+                    path.append(currentBoard)
+                return path[::-1][1:]  # Order reversed since traversing is started at leaf board
+
+            # Check all moves that could be made and add the new board to the queue
+            for newBoard in adj[currentBoard]:
+                # Check if you already have seen this board if so skip else add it to visit
+                if newBoard in visit:
+                    continue
+                visit.add(newBoard)
+                q.append(newBoard)
+                newBoard.parentBoard = currentBoard
+        depth += 1
+    return [board]
